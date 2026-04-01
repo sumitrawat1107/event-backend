dd2b42ef5a39c352a78a5fc831fc24399c75afbe
makesite <50260877+sumitrawat1107@users.noreply.github.com>
2026-04-01 22:56:23 +0530

    server.js

--- server.js
+++ server.js
@@ -1,38 +1,5 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// 🔗 MongoDB connect
-mongoose.connect("mongodb+srv://ssumitrawat1107_db_user:Rawat@123@cluster0.lohpjwf.mongodb.net/?appName=Cluster0")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
-// 📦 Schema
-const BookingSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-});
-
-const Booking = mongoose.model("Booking", BookingSchema);
-
-// 📩 POST API
-app.post("/book", async (req, res) => {
-  const newBooking = new Booking(req.body);
-  await newBooking.save();
-
-  res.json({ message: "Booking saved in DB!" });
-});
-
-// 📥 GET API
-app.get("/bookings", async (req, res) => {
-  const data = await Booking.find();
-  res.json(data);
-});
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server running");
 });
