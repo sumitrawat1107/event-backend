91f8d92d647d3ee407bf194e10be4f9cc206bfe0
makesite <50260877+sumitrawat1107@users.noreply.github.com>
2026-04-02 00:04:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const PORT = process.env.PORT || 5000;
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
+const app = express();   // 👈 yahan app define karna zaroori hai
+
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log(`Server running on port ${PORT}`);
 });
